6dddec5251cd2db7b0221ab44b180bda1eff43fe
Alexey Kuznetsov <axet@me.com>
2017-02-26 21:31:13 +0300

    move to library

--- src/main/java/com/github/axet/audiolibrary/app/Storage.java
+++ src/main/java/com/github/axet/audiolibrary/app/Storage.java
@@ -22,93 +22,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class Storage {
+public class Storage extends com.github.axet.androidlibrary.app.Storage {
     public static final String TMP_REC = "recorind.data";
     public static final String RECORDINGS = "recordings";
-
-    public static String getNameNoExt(File f) {
-        String fileName = f.getName();
-
-        int i = fileName.lastIndexOf('.');
-        if (i > 0) {
-            fileName = fileName.substring(0, i);
-        }
-        return fileName;
-    }
-
-    public static String getExt(File f) {
-        String fileName = f.getName();
-
-        int i = fileName.lastIndexOf('.');
-        if (i > 0) {
-            return fileName.substring(i + 1);
-        }
-        return "";
-    }
-
-    public static File getNextFile(File parent, File f) {
-        String fileName = f.getName();
-
-        String extension = "";
-
-        int i = fileName.lastIndexOf('.');
-        if (i > 0) {
-            extension = fileName.substring(i + 1);
-            fileName = fileName.substring(0, i);
-        }
-
-        return getNextFile(parent, fileName, extension);
-    }
-
-    public static File getNextFile(File parent, String name, String ext) {
-        String fileName;
-        if (ext == null || ext.isEmpty())
-            fileName = name;
-        else
-            fileName = String.format("%s.%s", name, ext);
-
-        File file = new File(parent, fileName);
-
-        int i = 1;
-        while (file.exists()) {
-            if (ext == null || ext.isEmpty())
-                fileName = String.format("%s (%d)", name, i);
-            else
-                fileName = String.format("%s (%d).%s", name, i, ext);
-            file = new File(parent, fileName);
-            i++;
-        }
-
-//        try {
-//            file.createNewFile();
-//        } catch (IOException e) {
-//            throw new RuntimeException("Unable to create: " + file, e);
-//        }
-
-        return file;
-    }
-
-    public static void delete(File f) {
-        f.delete();
-    }
-
-    public static void move(File f, File to) {
-        try {
-            InputStream in = new FileInputStream(f);
-            OutputStream out = new FileOutputStream(to);
-
-            byte[] buf = new byte[1024];
-            int len;
-            while ((len = in.read(buf)) > 0) {
-                out.write(buf, 0, len);
-            }
-            in.close();
-            out.close();
-            f.delete();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     protected Context context;
 
@@ -226,17 +142,6 @@ public class Storage {
         return free / perSec * 1000;
     }
 
-    public long getFree(File f) {
-        while (!f.exists())
-            f = f.getParentFile();
-
-        StatFs fsi = new StatFs(f.getPath());
-        if (Build.VERSION.SDK_INT < 18)
-            return fsi.getBlockSize() * (long) fsi.getAvailableBlocks();
-        else
-            return fsi.getBlockSizeLong() * fsi.getAvailableBlocksLong();
-    }
-
     public File getTempRecording() {
         File internal = new File(context.getApplicationInfo().dataDir, TMP_REC);
 
@@ -282,6 +187,5 @@ public class Storage {
             throw new RuntimeException(e);
         }
     }
-
 
 }
